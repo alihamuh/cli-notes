b4c1d54f7d61e70a0fd667f3aeb5f97422e40f99
alihamuh <alihamuh@gmail.com>
2020-02-26 04:09:24 -0500

    added a multiline method.

--- done.js
+++ done.js
@@ -534,6 +534,28 @@ function sortTags() {
   //console.log(data.tags);
 }
 
+function multiline() {
+  var readline = require("readline");
+
+  var input = [];
+
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.prompt();
+
+  rl.on("line", function(cmd) {
+    input.push(cmd);
+  });
+
+  rl.on("close", function(cmd) {
+    console.log(input.join("\n"));
+    process.exit(0);
+  });
+}
+
 //Entry point
 var command = process.argv[2];
 var argument = process.argv[3];
@@ -590,6 +612,9 @@ switch (command) {
   case "search":
     search(argument);
     //sortTags();
+    break;
+  case "test":
+    multiline();
     break;
   case undefined:
     list();
